a7a75bfc6c4bcd7b020ab0eacfad136ffd82e473
Jose Montilla <jose.montilla@gmail.com>
2024-05-11 07:14:04 -0400

    jenkins backup restore

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,20 +1,20 @@
-resource "helm_release" "jenkins" {
-  provider = helm
+# resource "helm_release" "jenkins" {
+#   provider = helm
 
-  name       = "jenkins"
-  repository = "https://charts.jenkins.io"
-  chart      = "jenkins"
-  # version    = "3.6.0"
-  namespace  = "jenkins"
-  timeout    = 600
-  values = [
-    file("values.yaml"),
-  ]
+#   name       = "jenkins"
+#   repository = "https://charts.jenkins.io"
+#   chart      = "jenkins"
+#   # version    = "3.6.0"
+#   namespace  = "jenkins"
+#   timeout    = 600
+#   values = [
+#     file("values.yaml"),
+#   ]
 
-  depends_on = [
-    kubernetes_namespace.jenkins
-  ]
-}
+#   depends_on = [
+#     kubernetes_namespace.jenkins
+#   ]
+# }
 
 resource "kubernetes_namespace" "jenkins" {
   provider = kubernetes
