c62add3f263a5c6b79a1136f52466150779a5005
Jose Montilla <jose.montilla@gmail.com>
2024-05-13 12:57:29 -0400

    Jcasc updates

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -25,8 +25,6 @@ module "vpc" {
   public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr_public , 4, k)]
 
   enable_nat_gateway     = true
-#   single_nat_gateway     = true
-#   one_nat_gateway_per_az = false
 
   enable_dns_hostnames = true
   enable_dns_support   = true
