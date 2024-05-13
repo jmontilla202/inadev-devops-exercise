ccc4a99cdf4405aaf843f8765290b21c9b3107b1
Jose Montilla <jose.montilla@gmail.com>
2024-05-13 08:24:42 -0400

    cleanup, refactor

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -35,15 +35,3 @@ module "vpc" {
     env = var.env
   }
 }
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "private_subnet_id1" {
-  value = module.vpc.private_subnets[0]
-}
-
-output "private_subnet_id2" {
-  value = module.vpc.private_subnets[1]
-}
