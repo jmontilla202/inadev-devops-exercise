654b4bf3db5e2b06aa9de2e3b448653a06f1cd9f
Jose Montilla <jose.montilla@gmail.com>
2024-05-14 19:08:48 -0400

    Jenkinsfile, Dockerfile updates

--- inadev-job.groovy
+++ inadev-job.groovy
@@ -1,53 +1,54 @@
 pipelineJob('wapi-app') {
-        triggers {
-            hudsonStartupTrigger {
-              nodeParameterName("Built-In Node")
-              label("master")
-              quietPeriod("60")
-              runOnChoice("False")
+            triggers {
+                hudsonStartupTrigger {
+                  nodeParameterName("Built-In Node")
+                  label("master")
+                  quietPeriod("60")
+                  runOnChoice("False")
+                }
             }
-        }
-    definition {
+            definition {
                 cps {
                   script("""
                       pipeline {
                         agent any
                             agent {
                               kubernetes {
-                                yaml '''
-                                      apiVersion: v1
-                                      kind: Pod
-                                      metadata:
-                                        labels:
-                                          app: wapi
-                                          version: v0.1
-                                      spec:
-                                        containers:     
-                                        - name: golang
-                                        image: golang:1.21.9
-                                        command:
-                                        - cat
-                                        tty: true
-                                        - name: docker
-                                        image: docker:latest
-                                        command:
-                                        - cat
-                                        tty: true
-                                        securityContext:
-                                          allowPrivilegeEscalation: true
-                                          runAsUser: 0
-                                          runAsGroup: 0
-                                          readOnlyRootFilesystem: false
-                                          privileged: true
-                                        '''
+                                yaml: 
+                                '''
+                                apiVersion: v1
+                                kind: Pod
+                                metadata:
+                                  labels:
+                                    app: wapi
+                                    version: v0.1
+                                spec:
+                                  containers:     
+                                  - name: golang
+                                  image: golang:1.21.9
+                                  command:
+                                  - cat
+                                  tty: true
+                                  - name: docker
+                                  image: docker:latest
+                                  command:
+                                  - cat
+                                  tty: true
+                                  securityContext:
+                                    allowPrivilegeEscalation: true
+                                    runAsUser: 0
+                                    runAsGroup: 0
+                                    readOnlyRootFilesystem: false
+                                    privileged: true
+                                  '''
                                 retries 2
                               }
                             }
                           environment {
                               GIT_SSH_COMMAND = 'ssh -o StrictHostKeyChecking=no' // Skip host key checking
-                              DOCKERHUB_CREDS = credentials('docker-hub')
-                              USERNAME = "${env.DOCKERHUB_CREDS_USR}"
-                              PASSWORD = "${env.DOCKERHUB_CREDS_PSW}"
+                              // DOCKERHUB_CREDS = credentials('docker-hub')
+                              // USERNAME = '${env.DOCKERHUB_CREDS_USR}'
+                              // PASSWORD = '${env.DOCKERHUB_CREDS_PSW}'
                           }
                           stages {
                               stage('Checkout') {
