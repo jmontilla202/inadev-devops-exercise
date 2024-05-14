ecd3ac94e0e53a01866737b00f91919f20ae161f
Jose Montilla <jose.montilla@gmail.com>
2024-05-14 18:02:09 -0400

    Jenkinsfile, Dockerfile updates

--- inadev-job.groovy
+++ inadev-job.groovy
@@ -10,44 +10,44 @@
         definition {
                 cps {
                   script("""            
-    pipeline {
-          agent {
-            kubernetes {
-              yaml '''
-                apiVersion: v1
-                kind: Pod
-                metadata:
-                  labels:
-                    app: wapi
-                    version: v0.1
-                spec:
-                  containers:
-                  - name: golang
-                    image: golang:1.21.9
-                    command:
-                    - cat
-                    tty: true
-                  - name: docker
-                    image: docker:latest
-                    command:
-                    - cat
-                    tty: true
-                    securityContext:
-                      allowPrivilegeEscalation: true
-                      runAsUser: 0
-                      runAsGroup: 0
-                      readOnlyRootFilesystem: false
-                      privileged: true
-                '''
-        retries 2
-      }
-          }
-        environment {
-            GIT_SSH_COMMAND = 'ssh -o StrictHostKeyChecking=no' // Skip host key checking
-               DOCKERHUB_CREDS = credentials('docker-hub')
-              USERNAME = "${env.DOCKERHUB_CREDS_USR}"
-             PASSWORD = "${env.DOCKERHUB_CREDS_PSW}"
-        }
+    // pipeline {
+    //       agent {
+    //         kubernetes {
+    //           yaml '''
+    //             apiVersion: v1
+    //             kind: Pod
+    //             metadata:
+    //               labels:
+    //                 app: wapi
+    //                 version: v0.1
+    //             spec:
+    //               containers:
+    //               - name: golang
+    //                 image: golang:1.21.9
+    //                 command:
+    //                 - cat
+    //                 tty: true
+    //               - name: docker
+    //                 image: docker:latest
+    //                 command:
+    //                 - cat
+    //                 tty: true
+    //                 securityContext:
+    //                   allowPrivilegeEscalation: true
+    //                   runAsUser: 0
+    //                   runAsGroup: 0
+    //                   readOnlyRootFilesystem: false
+    //                   privileged: true
+    //             '''
+    //     retries 2
+    //   }
+    //       }
+    //     environment {
+    //         GIT_SSH_COMMAND = 'ssh -o StrictHostKeyChecking=no' // Skip host key checking
+    //            DOCKERHUB_CREDS = credentials('docker-hub')
+    //           USERNAME = "${env.DOCKERHUB_CREDS_USR}"
+    //          PASSWORD = "${env.DOCKERHUB_CREDS_PSW}"
+    //     }
         stages {
             stage('Checkout') {
                 steps {
@@ -83,7 +83,8 @@
               }
           }
         }
-    }              """.stripIndent())
+    }              
+    """.stripIndent())
                  sandbox()
                  }
            }
