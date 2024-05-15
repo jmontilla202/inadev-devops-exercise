7ae0aa6dc0a5daee8dbe655a7d4da8640a4a1632
Jose Montilla <jose.montilla@gmail.com>
2024-05-14 20:29:10 -0400

    Jenkinsfile, Dockerfile updates

--- inadev-job.groovy
+++ inadev-job.groovy
@@ -4,7 +4,12 @@ pipelineJob('wapi-app') {
                   script("""
                       pipeline {
                           agent any
-
+                          environment {
+                              GIT_SSH_COMMAND = 'ssh -o StrictHostKeyChecking=no' // Skip host key checking
+                              DOCKERHUB_CREDS = credentials('docker-hub')
+                              USERNAME = "${env.DOCKERHUB_CREDS_USR}"
+                              PASSWORD = "${env.DOCKERHUB_CREDS_PSW}"
+                          }
                           stages {
                               stage('Checkout') {
                                   steps {
@@ -23,7 +28,7 @@ pipelineJob('wapi-app') {
                                   container('docker') {
                                     sh '''
                                         dockerd --iptables=false --tls=false --bridge=none -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --data-root /var/lib/docker &
-
+                                        docker login -u $USERNAME -p $PASSWORD
                                         sleep 10
                                         docker build -t jose9123/wapi:latest .
                                         docker images
