03a46b581349d21862d5bd977bdf7c7c22b95119
Jose Montilla <jose.montilla@gmail.com>
2024-05-15 00:40:30 -0400

    Resolved missing Property error - 3o

--- inadev-job.groovy
+++ inadev-job.groovy
@@ -65,13 +65,6 @@
                                   go build -buildvcs=false -o wapi
                                 '''
                               }
-                              container('golang') {
-                                sh '''
-                                  cd src
-                                  go get "github.com/gin-gonic/gin"
-                                  go build -buildvcs=false -o wapi
-                                '''
-                              }
                             }
                           }
                           stage('Deploy') {
