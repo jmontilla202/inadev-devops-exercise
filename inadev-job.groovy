97edf98ace10383af4cd7b6b30c07e37baecaac3
Jose Montilla <jose.montilla@gmail.com>
2024-05-15 00:04:02 -0400

    Resolved missing Property error - 3m

--- inadev-job.groovy
+++ inadev-job.groovy
@@ -67,16 +67,16 @@
                           }
                           stage('Build') {
                             steps {                             
-                              container('docker') {
-                                sh '''
-                                  dockerd --iptables=false --tls=false --bridge=none -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --data-root /var/lib/docker &
-                                  #docker login -u $USERNAME -p $PASSWORD
-                                  sleep 5
-                                  docker build -t jose9123/wapi:latest .
-                                  docker push jose9123/wapi:latest
-                                  docker images
-                                '''
-                              }
+                                container('docker') {
+                                  sh '''
+                                    dockerd --iptables=false --tls=false --bridge=none -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --data-root /var/lib/docker &
+                                    docker login -u $USERNAME -p $PASSWORD
+                                    sleep 3
+                                    docker build -t jose9123/wapi:latest .
+                                    docker push jose9123/wapi:latest
+                                    docker images
+                                  '''
+                                }
                             }
                           }
                           stage('Deploy') {
