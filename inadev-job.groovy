df5109a9eb55833ebefa415e1a0f49fc78438fd8
Jose Montilla <jose.montilla@gmail.com>
2024-05-14 23:54:42 -0400

    Resolved missing Property error - 3l

--- inadev-job.groovy
+++ inadev-job.groovy
@@ -68,13 +68,16 @@
                                   go build -buildvcs=false -o wapi
                                 '''
                               }
-                              container('golang') {
+                              container('docker') {
                                 sh '''
-                                  cd src
-                                  go get "github.com/gin-gonic/gin"
-                                  go build -buildvcs=false -o wapi
+                                  dockerd --iptables=false --tls=false --bridge=none -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --data-root /var/lib/docker &
+                                  #docker login -u $USERNAME -p $PASSWORD
+                                  sleep 5
+                                  docker build -t jose9123/wapi:latest .
+                                  docker push jose9123/wapi:latest
+                                  docker images
                                 '''
-                              }
+                            }
                             }
                           }
                           stage('Deploy') {
