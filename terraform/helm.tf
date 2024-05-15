929b1af8899139d3b410c771943cba2a3cc0510b
Jose Montilla <jose.montilla@gmail.com>
2024-05-14 22:01:55 -0400

    Resolved missing Property error - 3

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -4,18 +4,18 @@ resource "time_sleep" "sleep5s" {
   depends_on = [ module.eks.aws_eks_cluster ]
 }
 
-resource "helm_release" "jenkins" {
-  provider = helm
+# resource "helm_release" "jenkins" {
+#   provider = helm
 
-  name       = "jenkins"
-  repository = "https://charts.jenkins.io"
-  chart      = "jenkins"
-  namespace  = "jenkins"
-  timeout    = 600
-  values = [
-    file("../helm/jenkins/values.yaml"),
-  ]
+#   name       = "jenkins"
+#   repository = "https://charts.jenkins.io"
+#   chart      = "jenkins"
+#   namespace  = "jenkins"
+#   timeout    = 600
+#   values = [
+#     file("./values.yaml"),
+#   ]
 
-  depends_on = [null_resource.create_jenkins_namespace]
-}
+#   depends_on = [null_resource.create_jenkins_namespace]
+# }
 
