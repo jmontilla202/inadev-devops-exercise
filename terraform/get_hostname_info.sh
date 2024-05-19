e064d87659d7a2058e9663c9e2bdc4cdd537d226
Jose Montilla <jose.montilla@gmail.com>
2024-05-19 11:43:10 -0400

    cleanup, minor tweaks

--- terraform/get_hostname_info.sh
+++ terraform/get_hostname_info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export JENKINS_LB="Jenkins: http://$(kubectl get service -n jenkins jenkins -o jsonpath='{.status.loadBalancer.ingress[].hostname}'):8080 \n(username\password: ops/cicdops123)"
+export JENKINS_LB="Jenkins: http://$(kubectl get service -n jenkins jenkins -o jsonpath='{.status.loadBalancer.ingress[].hostname}'):8080"
 export WAPI_LB="Wapi: http://$(kubectl get service -n wapi wapi -o jsonpath='{.status.loadBalancer.ingress[].hostname}')/forecast"
 
-echo $JENKINS_LB
+echo "$JENKINS_LB - (username:password: ops:cicdops123) "
 echo $WAPI_LB
